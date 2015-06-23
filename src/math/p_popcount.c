e1ee6a53ea270bda61783377d40f67f7d863abcb
tedades <dddepasmans@gmail.com>
2015-06-24 00:23:15 +0200

    Update for p_popcount.c
    Changes:
    1. Changed int to unsigned int for parameter n, as the size will not be negative and unsigned is handled faster.
    2. Made the for loop count down instead of up, this removes one compare step per iteration in the loop ( (n-i)!=0 becomes n!=0 ).
    3. The for-loop change made didn't need variable 'i', so removed 'i' which will saves memory.
    
    What could be done more:
    1. I haven't run the code, and don't know the result of the code, but if the result is how many bits are set (so 0xFF = 8 bits set) the u64 function could use an uint32_t as result as a 64bit variable can only have 64bits set to 1 and the number 64 can even fit in int_fast8_t.

--- src/math/p_popcount.c
+++ src/math/p_popcount.c
@@ -13,19 +13,18 @@
  * @return      None
  *
  */
-void p_popcount_u32(const uint32_t *a, uint32_t *c, int n)
+void p_popcount_u32(const uint32_t *a, uint32_t *c, uint32_t n)
 {
     static const uint32_t A[] = {0x55555555, 0x33333333,
                                  0x0f0f0f0f, 0x01010101};
     const uint32_t *pa;
     uint32_t *pc;
     uint32_t tmp;
-    int i;
 
     pa = a;
     pc = c;
 
-    for (i = 0; i < n; i++) {
+    for (n; n != 0; n--) {
         tmp = *pa - ((*pa >> 1) & A[0]);
         tmp = (tmp & A[1]) + ((tmp >> 2) & A[1]);
         *pc = ((tmp + (tmp >> 4)) & A[2]) * A[3] >> 24;
@@ -34,19 +33,18 @@ void p_popcount_u32(const uint32_t *a, uint32_t *c, int n)
     }
 }
 
-void p_popcount_u64(const uint64_t *a, uint64_t *c, int n)
+void p_popcount_u64(const uint64_t *a, uint64_t *c, uint64_t n)
 {
     static const uint64_t A[] = {0x5555555555555555, 0x3333333333333333,
                                  0x0f0f0f0f0f0f0f0f, 0x0101010101010101};
     const uint64_t *pa;
     uint64_t *pc;
     uint64_t tmp;
-    int i;
 
     pa = a;
     pc = c;
 
-    for (i = 0; i < n; i++) {
+    for (n; n != 0; n--) {
         tmp = *pa - ((*pa >> 1) & A[0]);
         tmp = (tmp & A[1]) + ((tmp >> 2) & A[1]);
         *pc = ((tmp + (tmp >> 4)) & A[2]) * A[3] >> 56;
